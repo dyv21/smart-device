397900ecfc681b99e6242551711a4ba212abbec1
dyv21 <d.y.vladimirov@gmail.com>
2021-02-04 11:10:42 +0300

    modal

--- build/js/script.js
+++ build/js/script.js
@@ -16,8 +16,36 @@ const accordeonList = document.querySelectorAll('.accordeon__item');
 
 accordeonList.forEach((item) => {
   item.addEventListener('click', function () {
+    this.classList.toggle('accordeon__item');
     this.classList.toggle('accordeon__item--active');
-
   });
 });
 
+const modal = document.querySelector('.modal')
+const modalShow = document.querySelector('.main-nav__item:last-child');
+const modalClose = modal.querySelector('.modal__close')
+
+const closePopup = () => {
+  if (modal.classList.contains('modal--show')) {
+    modal.classList.remove('modal--show');
+  };
+}
+
+modalShow.addEventListener('click', () => {
+  modal.classList.add('modal--show');
+});
+
+modal.addEventListener ('click', (evt) => {
+  if (!evt.target.closest('.modal__window')) {
+    closePopup();
+  };
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  };
+});
+
+modalClose.addEventListener('click', closePopup);
